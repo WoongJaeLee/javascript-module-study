7bf41b35896012c5de00b654cbef02c36512fa86
이웅재(Woongjae Lee)/Back Office개발팀/11ST <lwj0513@sk.com>
2019-05-19 19:15:09 +0900

    Initial commit

---
+++ objectListerals/objectLiterals.js
@@ -0,0 +1,13 @@
+var myModule = {
+
+    someValue: "value",
+    someVoidFunction: function() {
+        console.log("im void function like java");
+    },
+    someReturnFunction: function() {
+        return "im return string value";
+    },
+    someFunctionWithParams: function (msg) {
+        console.log("im params function :" + msg);
+    }
+};
